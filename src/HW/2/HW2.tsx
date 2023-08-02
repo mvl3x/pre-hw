ba06ad730b6c215942338e08f13b129f0430ebf8
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-08-02 20:28:03 +0500

    fix: fix jsx

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -30,19 +30,69 @@ export const HW2 = () => {
   // ❗ Массив с данными не трогаем!
 
   const users = {
-    myFriends:[
-      { id: 1, name: 'John', age: 25, address: { street: '123 Main St', city: 'New York' } },
-      { id: 2, name: 'Alice', age: 30, address: { street: '456 Elm St', city: 'San Francisco' } },
-      { id: 3, name: 'Bob', age: 35, address: { street: '789 Oak St', city: 'Seattle' } },
-      { id: 4, name: 'John', age: 44, address: { street: '123 Maple Ave', city: 'Dallas' } },
-      { id: 5, name: 'Mary', age: 13, address: { street: '456 Elm St', city: 'Los Angeles' } },
-      { id: 6, name: 'James', age: 18, address: { street: '987 Pine Rd', city: 'Dallas' } },
-      { id: 7, name: 'Jennifer', age: 21, address: { street: '654 Birch Ct', city: 'Seattle' } },
-      { id: 8, name: 'Robert', age: 4, address: { street: '876 Spruce Way', city: 'San Francisco' } },
-      { id: 9, name: 'Jessica', age: 12, address: { street: '543 Willow Dr', city: 'San Francisco' } },
-      { id: 10, name: 'Emily', age: 55, address: { street: '765 Aspen Blvd', city: 'Los Angeles' } },
-    ]
-  }
+    myFriends: [
+      {
+        id: 1,
+        name: "John",
+        age: 25,
+        address: { street: "123 Main St", city: "New York" },
+      },
+      {
+        id: 2,
+        name: "Alice",
+        age: 30,
+        address: { street: "456 Elm St", city: "San Francisco" },
+      },
+      {
+        id: 3,
+        name: "Bob",
+        age: 35,
+        address: { street: "789 Oak St", city: "Seattle" },
+      },
+      {
+        id: 4,
+        name: "John",
+        age: 44,
+        address: { street: "123 Maple Ave", city: "Dallas" },
+      },
+      {
+        id: 5,
+        name: "Mary",
+        age: 13,
+        address: { street: "456 Elm St", city: "Los Angeles" },
+      },
+      {
+        id: 6,
+        name: "James",
+        age: 18,
+        address: { street: "987 Pine Rd", city: "Dallas" },
+      },
+      {
+        id: 7,
+        name: "Jennifer",
+        age: 21,
+        address: { street: "654 Birch Ct", city: "Seattle" },
+      },
+      {
+        id: 8,
+        name: "Robert",
+        age: 4,
+        address: { street: "876 Spruce Way", city: "San Francisco" },
+      },
+      {
+        id: 9,
+        name: "Jessica",
+        age: 12,
+        address: { street: "543 Willow Dr", city: "San Francisco" },
+      },
+      {
+        id: 10,
+        name: "Emily",
+        age: 55,
+        address: { street: "765 Aspen Blvd", city: "Los Angeles" },
+      },
+    ],
+  };
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
@@ -52,7 +102,7 @@ export const HW2 = () => {
   };
 
   return (
-    <div id={'hw02'}>
+    <div id={"hw02"}>
       <UserList2 users={currentUsers} filterUsers={filterUsers} />
     </div>
   );
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -7,17 +7,19 @@ type UserList2PropsType = {
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
-	return (
-		<div id={'hw02-users'}>
-			<h2>User List:</h2>
+  return (
+    <div id={"hw02-users"}>
+      <h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+      <button id={"hw02-filter-button"} onClick={props.filterUsers}>
+        SHOW ME FRIENDS FROM LA
+      </button>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
-		</div>
-	);
+      <ul>
+        {props.users.myFriends.map((user) => (
+          <CurrentUser key={user.id} user={user} />
+        ))}
+      </ul>
+    </div>
+  );
 };
